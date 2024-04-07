0ebcbce94430af3da83b3a7080c2c2ae54dc6112
Cleve Littlefield <cleve@pulumi.com>
2024-04-07 02:18:34 -0700

    More features

--- src/esc/env_tree_data_provider.ts
+++ src/esc/env_tree_data_provider.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import EscApi from './api';
 import * as cli from './cli';
-import { formEnvUri } from './environmentUri';
+import { formEnvUri, parseEnvUri } from './environmentUri';
 
 
 export function trackEnvironmentEditorSelection(escTreeProvider: EnvironmentsTreeDataProvider, treeView: vscode.TreeView<any>): (e: vscode.TextEditor | undefined) => any {
@@ -19,11 +19,11 @@ export function onOpenLanguageSelector(): (e: vscode.TextDocument) => any {
     return async (document) => {
         if (document.uri.scheme === "pulumi" && document.languageId !== "yaml") {
             if (document.uri.path.endsWith("json")) {
-                await vscode.languages.setTextDocumentLanguage(document, "yaml");
+                await vscode.languages.setTextDocumentLanguage(document, "json");
                 return;
             }
             if (document.uri.path.endsWith("dotenv") || document.uri.path.endsWith("shell")) {
-                await vscode.languages.setTextDocumentLanguage(document, "shell");
+                await vscode.languages.setTextDocumentLanguage(document, "shellscript");
                 return;
             }
             await vscode.languages.setTextDocumentLanguage(document, "yaml");
@@ -32,7 +32,7 @@ export function onOpenLanguageSelector(): (e: vscode.TextDocument) => any {
 }
   
  
-export class EnvironmentsTreeDataProvider implements vscode.TreeDataProvider<Environment> {
+export class EnvironmentsTreeDataProvider implements vscode.TreeDataProvider<Environment | Revision> {
     private _onDidChangeTreeData: vscode.EventEmitter<any> = new vscode.EventEmitter<any>();
     public onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
     private data = new Map<string, Environment>();
@@ -43,32 +43,60 @@ export class EnvironmentsTreeDataProvider implements vscode.TreeDataProvider<Env
           });
     }
 
-    getTreeItem(element: Environment): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    getTreeItem(element: Environment | Revision): vscode.TreeItem | Thenable<vscode.TreeItem> {
         return element;
     }
 
-    async getChildren(element?: Environment): Promise<Environment[]> {
-        if (element) {
-            return [];
+    async getChildren(element?: Environment | Revision): Promise<Environment[]> {
+        if (element && element instanceof Environment) {
+            return await this.getRevisions(element);
         }
 
+        return await this.getEnvironments();
+    }
+
+    private async getRevisions(element: Environment) {
+        const { org, envName } = parseEnvUri(element.resourceUri!);
+        const api = new EscApi(org);
+
+        const [revisions, tags] = await Promise.all([api.listRevisions(envName), api.listTags(envName)]);
+
+        const revTagMap = new Map<number, string[]>();
+        tags.forEach((tag) => {
+            const tags = revTagMap[tag.revision] || [];
+            tags.push(tag.name);
+            revTagMap[tag.revision] = tags;
+        });
+        const revItems = revisions.map(rev => new Revision(org, envName, rev.number, element, revTagMap[rev.number] || [], vscode.TreeItemCollapsibleState.None));
+        revItems.forEach(item => this.mapResourceUris(item));
+
+        return revItems;
+    }
+
+    private async getEnvironments() {
         const org = await cli.organization();
         const api = new EscApi(org);
 
         const environments = await api.listAllEnvironments();
-        const envItems = environments.map(env => new Environment(org, env.name, vscode.TreeItemCollapsibleState.None));
-        envItems.forEach(element => {
-            if (element.resourceUri) {
-                this.data.set(element.resourceUri.toString(), element);
-            }
-        });
+        const envItems = environments.map(env => new Environment(org, env.name, vscode.TreeItemCollapsibleState.Collapsed));
+        envItems.forEach(item => this.mapResourceUris(item));
 
         envItems.sort((a, b) => a.envName.localeCompare(b.envName));
 
         return envItems;
     }
 
-    async getParent(element: Environment): Promise<Environment | undefined> {
+    private mapResourceUris(element: Environment) {
+        if (element.resourceUri) {
+            this.data.set(element.resourceUri.toString(), element);
+        }
+    }
+
+    async getParent(element: Environment | Revision): Promise<Environment | undefined> {
+        if (element instanceof Revision) {
+            return element.parent;
+        }
+
         return undefined;
     }
 
@@ -86,11 +114,35 @@ export class Environment extends vscode.TreeItem {
     ) {
         super(envName, collapsibleState);
         this.label = envName;
-        this.iconPath = new vscode.ThemeIcon('symbol-keyword');
         const uri = formEnvUri(org, envName);
         this.id = `env-${org}-${envName}`;
         this.resourceUri = uri;
         this.contextValue = 'environment';
+        this.command = {
+            command: 'vscode.open',
+            title: 'Edit Environment',
+            arguments: [uri],
+        };
+    }
+}
+
+export class Revision extends vscode.TreeItem {
+    constructor(
+        public readonly org: string,
+        public readonly envName: string,
+        public readonly revision: number,
+        public readonly parent: Environment,
+        public readonly tags: string[],
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    ) {
+        super(envName, collapsibleState);
+        this.label = revision.toString();
+        this.description = this.tags.join(",");
+        const base = formEnvUri(org, envName);
+        const uri = vscode.Uri.joinPath(base, "revision", revision.toString());
+        this.id = `env-${org}-${envName}-${revision}`;
+        this.resourceUri = uri;
+        this.contextValue = 'revision';
         this.command = {
             command: 'vscode.open',
             title: 'Edit Environment',
--- src/esc/index.ts
+++ src/esc/index.ts
@@ -1,14 +1,14 @@
 import * as vscode from 'vscode';
-import { EnvironmentsTreeDataProvider, trackEnvironmentEditorSelection, onOpenLanguageSelector, Environment } from './env_tree_data_provider';
+import { EnvironmentsTreeDataProvider, trackEnvironmentEditorSelection, onOpenLanguageSelector } from './env_tree_data_provider';
 import { EnvironmentFileSystemProvider } from './environment_fs_provider';
-import { addEnvironmentCommand, decryptEnvironmentCommand, deleteEnvironmentCommand, openEnvironmentCommand } from './commands';
+import { addEnvironmentCommand, decryptEnvironmentCommand, deleteEnvironmentCommand, openEnvironmentCommand, compareFilesCommands, tagRevisionCommand } from './commands';
 
 export function pulumiEscExplorer(context: vscode.ExtensionContext) {
     const escTreeProvider = new EnvironmentsTreeDataProvider(context);
     const escEnvironmentProvider = new EnvironmentFileSystemProvider();
     const onOpen = vscode.workspace.onDidOpenTextDocument(onOpenLanguageSelector());
-    const treeView = vscode.window.createTreeView("pulumi-esc-explorer", { treeDataProvider: escTreeProvider })
-    const fs = vscode.workspace.registerFileSystemProvider('pulumi', escEnvironmentProvider, { isCaseSensitive: true })
+    const treeView = vscode.window.createTreeView("pulumi-esc-explorer", { treeDataProvider: escTreeProvider });
+    const fs = vscode.workspace.registerFileSystemProvider('pulumi', escEnvironmentProvider, { isCaseSensitive: true });
     const docProvider = vscode.workspace.registerTextDocumentContentProvider('pulumi', escEnvironmentProvider);
 
     const addEnvCommand = addEnvironmentCommand();
@@ -16,25 +16,7 @@ export function pulumiEscExplorer(context: vscode.ExtensionContext) {
     const deleteEnvCommand = deleteEnvironmentCommand();
     const decryptEnvCommand = decryptEnvironmentCommand();
     const compareFiles = compareFilesCommands();
+    const tagRevision = tagRevisionCommand();
     const trackActiveEnv = vscode.window.onDidChangeActiveTextEditor(trackEnvironmentEditorSelection(escTreeProvider, treeView));
-    context.subscriptions.push(treeView, docProvider, onOpen, fs, trackActiveEnv, addEnvCommand, openEnvCommand, deleteEnvCommand, decryptEnvCommand, compareFiles);
-}
-
-function compareFilesCommands(): vscode.Disposable {
-
-  let selected: Environment | undefined;
-  const selectForCompare = vscode.commands.registerCommand('pulumi.esc.selectForCompare', async (env: Environment) => {
-    selected = env;
-    vscode.commands.executeCommand('setContext', 'pulumi.esc.compareEnabled', true);
-  });
-
-  const compareWithSelected = vscode.commands.registerCommand('pulumi.esc.compareWithSelected', async (env: Environment) => {
-    if (selected) {
-      vscode.commands.executeCommand('vscode.diff', selected.resourceUri, env.resourceUri);
-      vscode.commands.executeCommand('setContext', 'pulumi.esc.compareEnabled', false);
-      selected = undefined;
-    }
-  });
-
-  return vscode.Disposable.from(selectForCompare, compareWithSelected);
+    context.subscriptions.push(treeView, docProvider, onOpen, fs, trackActiveEnv, addEnvCommand, openEnvCommand, deleteEnvCommand, decryptEnvCommand,tagRevision, compareFiles);
 }
