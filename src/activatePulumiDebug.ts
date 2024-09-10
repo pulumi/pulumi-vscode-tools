b42ca5952c5329ee154ff0a648c7e92325610108
Eron Wright <eron@pulumi.com>
2024-09-09 17:07:16 -0700

    interactive stack selection

--- src/activatePulumiDebug.ts
+++ src/activatePulumiDebug.ts
@@ -10,9 +10,11 @@
 import * as vscode from 'vscode';
 import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
 import { FileAccessor, PulumiDebugSession } from './pulumiDebug';
+import { LocalWorkspace } from "@pulumi/pulumi/automation";
+
+const NEW_STACK_TEXT = 'Create a new stack...';
 
 export function activatePulumiDebug(context: vscode.ExtensionContext) {
-
 	// register configuration providers for 'pulumi' debug type
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('pulumi', new PulumiConfigurationProvider()));
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('pulumi', new PulumiDynamicConfigurationProvider(), 
@@ -23,25 +25,87 @@ export function activatePulumiDebug(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('pulumi', factory));
 }
 
+async function pickStack(workspaceFolder: string): Promise<string | undefined> {
+	// create (or select if one already exists) a stack that uses our local program
+	const ws = await LocalWorkspace.create({
+		workDir: workspaceFolder,
+	});
+
+	// use the current stack if there is one
+	// const current = await ws.stack();
+	// if (current) {
+	// 	return current.name;
+	// }
+
+	// select or create a stack
+	const stacks = await ws.listStacks({});
+	const picks = stacks.map<vscode.QuickPickItem>(s => {
+		return {
+			label: s.name,
+			description: s.current ? `(current)` : ``,
+			detail: s.url,
+		};
+	}).concat({label: '', kind: vscode.QuickPickItemKind.Separator}).concat({ label: NEW_STACK_TEXT });
+
+	const picked = await vscode.window.showQuickPick(picks, {
+		placeHolder: 'Select a stack'
+	});
+
+	if (picked?.label === NEW_STACK_TEXT) {
+		const stackName = await vscode.window.showInputBox({
+			placeHolder: 'dev',
+			prompt: 'Enter a name for the new stack',
+			title: 'Create a new stack',
+			validateInput: (value) => {
+				if (!value) {
+					return 'Stack name must not be empty';
+				}
+				if (stacks.find(s => s.name === value)) {
+					return 'A stack with this name already exists';
+				}
+				return undefined;
+			},
+		});
+		if (!stackName) {
+			return;
+		}
+		await ws.createStack(stackName);
+		return stackName;
+	}
+	if (picked) {
+		await ws.selectStack(picked.label);
+	}
+	return picked?.label;
+}
+
 class PulumiConfigurationProvider implements vscode.DebugConfigurationProvider {
 	/**
 	 * Resolve a debug configuration just before a debug session is being launched,
 	 * e.g. add all missing attributes to the debug configuration.
 	 */
 	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
-		// if (!config.stackName || config.stackName === '') {
-		// 	return vscode.window.showInformationMessage("Stack name must be configured").then(_ => {
-		// 		return undefined;	// abort launch
-		// 	});
-		// }
-		if (!config.stack) {
-			config.stack = 'dev';
-		}
 		if (!config.workDir) {
 			config.workDir = '${workspaceFolder}';
 		}
-
 		return config;
+	}
+
+	resolveDebugConfigurationWithSubstitutedVariables(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+		const workDir = config.workDir;
+		if (!workDir) {
+			return undefined;
+		}
+		const stackName = config.stackName;
+		if (stackName) {
+			return config;
+		}
+		return pickStack(workDir).then(stackName => {
+			if (!stackName) {
+				return undefined;
+			}
+			config.stackName = stackName;
+			return config;
+		});
 	}
 }
 
@@ -58,19 +122,17 @@ class PulumiDynamicConfigurationProvider implements vscode.DebugConfigurationPro
 				{
 					"type": "pulumi",
 					"request": "launch",
-					"name": "pulumi preview -s dev",
+					"name": "pulumi preview",
 					"command": "preview",
 					"workDir": "${workspaceFolder}",
-					"stackName": "dev",
 					"stopOnEntry": true
 				},
 				{
 					"type": "pulumi",
 					"request": "launch",
-					"name": "pulumi up -s dev",
+					"name": "pulumi up",
 					"command": "up",
 					"workDir": "${workspaceFolder}",
-					"stackName": "dev",
 					"stopOnEntry": true
 				}
 			];
--- src/extension.ts
+++ src/extension.ts
@@ -11,17 +11,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pulumi-vscode-tools" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	context.subscriptions.push(
-		vscode.commands.registerCommand('pulumi-vscode-tools.helloWorld', () => {
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
-			vscode.window.showInformationMessage('Hello World from Pulumi Tools!');
-		})
-	);
-
 	activatePulumiDebug(context);
 }
 
