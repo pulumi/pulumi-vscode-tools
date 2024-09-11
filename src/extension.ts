16663fbe11925daee4715d54fc353272f6387368
Eron Wright <eron@pulumi.com>
2024-09-11 15:07:33 -0700

    remove template code

--- src/extension.ts
+++ src/extension.ts
@@ -6,11 +6,6 @@ import { activatePulumiDebug } from './activatePulumiDebug';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "pulumi-vscode-tools" is now active!');
-
 	activatePulumiDebug(context);
 }
 
