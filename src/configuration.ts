617e1271da5f4e0263c0089b530f4523f460c87e
Eron Wright <eron@pulumi.com>
2024-09-11 09:45:13 -0700

    CLI auto-installation support

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,11 +1,66 @@
 
 import * as vscode from 'vscode';
-import { LocalWorkspace, LocalWorkspaceOptions } from "@pulumi/pulumi/automation";
+import { LocalWorkspace, LocalWorkspaceOptions, PulumiCommand } from "@pulumi/pulumi/automation";
+import { SemVer } from 'semver';
+import path from 'path';
+import * as os from "os";
 
-export async function createWorkspace(workdir: string, opts?: LocalWorkspaceOptions) {
+const minVersion: SemVer = new SemVer('v3.131.1-alpha.x86f5a4e');
+
+/**
+ * Ensures that the Pulumi CLI is installed and has a compatible version.
+ * @returns The Pulumi CLI command if it is installed, otherwise undefined.
+ */
+export async function checkPulumiInstallation(): Promise<PulumiCommand|undefined> {
+	let root = vscode.workspace.getConfiguration().get<string|undefined>('pulumi.root');
+	try {
+		// try the configured location and the PATH
+		return await PulumiCommand.get({root: root, version: minVersion, skipVersionCheck: false});
+	} catch (err) {
+	}
+	if (!root) {
+		// try the default install location of ~/.pulumi
+        root = path.join(os.homedir(), ".pulumi");
+		try {
+			return await PulumiCommand.get({root: root, version: minVersion, skipVersionCheck: false});
+		} catch (err) {
+		}
+    }
+	const selection = await vscode.window.showWarningMessage('Pulumi CLI is not installed.', 'Install', 'Cancel');
+		switch (selection) {
+			case 'Install':
+				return await installPulumi(root, minVersion);
+			default:
+				return undefined;
+		}
+}
+
+async function installPulumi(root?: string, version?: SemVer): Promise<PulumiCommand> {
+	return await vscode.window.withProgress({
+		location: vscode.ProgressLocation.Notification,
+		title: "Installing Pulumi CLI...",
+		cancellable: false
+	}, async (progress, token) => {
+		return await PulumiCommand.install({root: root, version: version, skipVersionCheck: false});
+	});
+}
+
+/**
+ * Create a new Pulumi local workspace.
+ * @param workdir The working directory of the workspace.
+ * @param opts Additional options for the workspace.
+ */
+export async function createWorkspace(workdir: string, opts?: LocalWorkspaceOptions): Promise<LocalWorkspace> {
+
+	const cli = await checkPulumiInstallation();
+	if (!cli) {
+		throw new Error('Pulumi CLI is not installed');
+	}
+
 	const envs = vscode.workspace.getConfiguration().get<{}>('pulumi.env');
-    const ws = await LocalWorkspace.create({
+	const ws = await LocalWorkspace.create({
 		...opts,
+		pulumiCommand: cli,
 		workDir: workdir,
 		envVars: {...envs, ...opts?.envVars},
 	});
